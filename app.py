396175c109ce8fa29f7f71cee0c77119b5004d5e
kleber.barilli <kleber.barilli73@gmail.com>
2022-09-25 23:18:31 -0300

    unlink file

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app.secret_key="123"
 
 app.config["UPLOAD_FOLDER"]="static/excel"
 
+def unlink_file(file_path):
+    return os.unlink(file_path)
+
 @app.route("/",methods=['GET','POST'])
 def upload():
     if request.method == 'POST':
@@ -15,6 +18,7 @@ def upload():
             file_path = os.path.join(app.config["UPLOAD_FOLDER"], upload_file.filename)
             upload_file.save(file_path)
             data=pd.read_excel(upload_file)
+            unlink_file(file_path)
             return render_template("ExcelFile.html",data=data.to_html(index=False).replace('<th>','<th style="text-align:center">'))
     return render_template("UploadExcel.html")
 
