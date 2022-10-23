a82b2e554490f9769f31eac2a976592c4ee5d7b8
kleber.barilli <kleber.barilli73@gmail.com>
2022-10-23 11:39:02 -0300

    install dotenv

--- app.py
+++ app.py
@@ -1,32 +1,35 @@
-from flask import Flask,render_template,request,jsonify
 import os
 import pandas as pd
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask, render_template, request, jsonify
 from src.controller.prepare_excel_data import PrepareExcelData
 
-app=Flask(__name__)
-app.secret_key="123"
+load_dotenv(find_dotenv())
 
-app.config["UPLOAD_FOLDER"]="tmp"
-
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+app.config["UPLOAD_FOLDER"] = "tmp"
 
 
 def unlink_file(file_path):
     return os.unlink(file_path)
 
-@app.route("/",methods=['GET','POST'])
+
+@app.route("/", methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         upload_file = request.files['upload_excel']
         if upload_file.filename != '':
-            file_path = os.path.join(app.config["UPLOAD_FOLDER"], upload_file.filename)
+            file_path = os.path.join(
+                app.config["UPLOAD_FOLDER"], upload_file.filename)
             upload_file.save(file_path)
-            data=pd.read_excel(upload_file)
+            data = pd.read_excel(upload_file)
             p = PrepareExcelData(data)
             p.prepare()
             unlink_file(file_path)
-            return render_template("imported-excel-data.html",data=data.to_html(index=False).replace('<th>','<th style="text-align:center">'))
+            return render_template("imported-excel-data.html", data=data.to_html(index=False).replace('<th>', '<th style="text-align:center">'))
     return render_template("upload-excel.html")
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
